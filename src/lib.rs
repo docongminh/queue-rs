ee33faea0ff21e402dd3603b13a756a3cbf239d7
docongminh <cristiando0902@gmail.com>
2023-03-12 11:16:04 +0700

    queue rs

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use std::{task::{Waker, Poll, Context}, time::Duration};
+
+use tokio_util::time::{DelayQueue, delay_queue::{Expired, Key}};
+
+pub struct PatientQueue<T> {
+    inner: DelayQueue<T>,
+    waker: Option<Waker>,
+}
+
+impl<T> Default for PatientQueue<T> {
+    fn default() -> Self {
+        Self {
+            inner: DelayQueue::new(),
+            waker: None,
+        }
+    }
+}
+
+impl<T> PatientQueue<T> {
+    pub fn insert(&mut self, value: T, timeout: Duration) -> Key {
+        if let Some(w) = self.waker.take() {
+            w.wake()
+        }
+
+        self.inner.insert(value, timeout)
+    }
+
+    pub fn poll_expired(&mut self, cx: &mut Context<'_>) -> Poll<Expired<T>> {
+        match self.inner.poll_expired(cx) {
+            Poll::Ready(Some(entry)) => Poll::Ready(entry),
+            Poll::Ready(None) => {
+                self.waker = Some(cx.waker().clone());
+                Poll::Pending
+            }
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
